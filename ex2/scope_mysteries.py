bcbe44862a8cfa6e2215caa1281a98e4f4ce22d9
dioppolo <diego.ioppolo2006@gmail.com>
2026-06-16 15:20:40 +0200

    ex2

---
+++ ex2/scope_mysteries.py
@@ -0,0 +1,69 @@
+from typing import Callable, Any
+
+
+def mage_counter() -> Callable:
+    counter = 0
+
+    def increment_counter() -> int:
+        nonlocal counter
+        counter += 1
+        return counter
+    return increment_counter
+
+
+def spell_accomulator(initial_power: int) -> Callable:
+    power = initial_power
+
+    def add_power(add: int) -> int:
+        nonlocal power
+        power = power + add
+        return power
+    return add_power
+
+
+def enchantment_factory(enchantment_type: str) -> Callable:
+
+    def item_name(name: str) -> str:
+        item = enchantment_type + " " + name
+        return item
+    return item_name
+
+
+def memory_vault() -> dict[str, Callable]:
+    vault = {}
+
+    def store(key: str, value: Any) -> str:
+        vault[key] = value
+        return f"{key} stored"
+
+    def recall(key: str) -> Any:
+        return vault.get(key, "Memory not found")
+    return {'store': store, 'recall': recall}
+
+
+def main():
+    count = mage_counter()
+    print(count())
+    print("=" * 40)
+    pow = spell_accomulator(10)
+    print(pow(0))
+    print(pow(25))
+    print("=" * 40)
+    flaming = enchantment_factory("flaming")
+    print(flaming("sword"))
+    print(flaming("staff"))
+    water = enchantment_factory("Water")
+    print(water("orb"))
+    print("=" * 40)
+    vault = memory_vault()
+    store_spell = vault['store']
+    recall_spell = vault['recall']
+    print("Storing Fireball:", store_spell("Fireball", 85))
+    print("Storing Teleport:", store_spell("Teleport", "Need Mana"))
+    print("Recalling Fireball:", recall_spell("Fireball"))
+    print("Recalling Teleport:", recall_spell("Teleport"))
+    print("Recalling Iceball:", recall_spell("IceBall"))
+
+
+if __name__ == "__main__":
+    main()
